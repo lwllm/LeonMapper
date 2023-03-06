118c762151c3119f5fa128680a330b72d46ab2d2
luowuliang <luowuliang1@beisen.com>
2023-03-06 19:56:02 +0800

    添加float相关基础类型转换

---
+++ LeonMapper/Convert/Converters/FloatConverters.cs
@@ -0,0 +1,161 @@
+﻿namespace LeonMapper.Convert.Converters;
+
+public class FloatToSbyteConverter : IConverter<float, sbyte>
+{
+    public sbyte Convert(float input)
+    {
+        if (input < sbyte.MinValue || input > sbyte.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (sbyte)input;
+    }
+}
+
+public class FloatToByteConverter : IConverter<float, byte>
+{
+    public byte Convert(float input)
+    {
+        if (input < byte.MinValue || input > byte.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (byte)input;
+    }
+}
+
+public class FloatToShortConverter : IConverter<float, short>
+{
+    public short Convert(float input)
+    {
+        if (input < short.MinValue || input > short.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (short)input;
+    }
+}
+
+public class FloatToUshortConverter : IConverter<float, ushort>
+{
+    public ushort Convert(float input)
+    {
+        if (input < ushort.MinValue || input > ushort.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (ushort)input;
+    }
+}
+
+public class FloatToIntConverter : IConverter<float, int>
+{
+    public int Convert(float input)
+    {
+        if (input < int.MinValue || input > int.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (int)input;
+    }
+}
+
+public class FloatToUintConverter : IConverter<float, uint>
+{
+    public uint Convert(float input)
+    {
+        if (input < uint.MinValue || input > uint.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (uint)input;
+    }
+}
+
+public class FloatToLongConverter : IConverter<float, long>
+{
+    public long Convert(float input)
+    {
+        if (input < long.MinValue || input > long.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (long)input;
+    }
+}
+
+public class FloatToUlongConverter : IConverter<float, ulong>
+{
+    public ulong Convert(float input)
+    {
+        if (input < ulong.MinValue || input > ulong.MaxValue)
+        {
+            throw new OverflowException();
+        }
+
+        return (ulong)input;
+    }
+}
+
+public class FloatToDoubleConverter : IConverter<float, double>
+{
+    public double Convert(float input)
+    {
+        return input;
+    }
+}
+
+public class FloatToDecimalConverter : IConverter<float, decimal>
+{
+    public decimal Convert(float input)
+    {
+        return (decimal)input;
+    }
+}
+
+public class FloatToCharConverter : IConverter<float, char>
+{
+    public char Convert(float input)
+    {
+        return (char)input;
+    }
+}
+
+public class FloatToBoolConverter : IConverter<float, bool>
+{
+    public bool Convert(float input)
+    {
+        return input != 0f;
+    }
+}
+
+public class FloatToIntPtrConverter : IConverter<float, IntPtr>
+{
+    public IntPtr Convert(float input)
+    {
+        return new IntPtr((int)input);
+    }
+}
+
+public class FloatToUIntPtrConverter : IConverter<float, UIntPtr>
+{
+    public UIntPtr Convert(float input)
+    {
+        return new UIntPtr((uint)input);
+    }
+}
+
+public class FloatToStringConverter : IConverter<float, string>
+{
+    public string Convert(float input)
+    {
+        return input.ToString();
+    }
+}
--- LeonMapper/Convert/Converters/ULongConverters.cs
+++ LeonMapper/Convert/Converters/ULongConverters.cs
@@ -52,7 +52,7 @@ public class ULongToIntConverter : IConverter<ulong, int>
 {
     public int Convert(ulong input)
     {
-        if (input > (ulong)int.MaxValue)
+        if (input > int.MaxValue)
         {
             throw new OverflowException();
         }
@@ -76,7 +76,7 @@ public class ULongToLongConverter : IConverter<ulong, long>
 {
     public long Convert(ulong input)
     {
-        if (input > (ulong)long.MaxValue)
+        if (input > long.MaxValue)
         {
             throw new OverflowException();
         }
@@ -84,3 +84,81 @@ public class ULongToLongConverter : IConverter<ulong, long>
     }
 }
 
+public class ULongToFloatConverter : IConverter<ulong, float>
+{
+    public float Convert(ulong input)
+    {
+        if (input > float.MaxValue)
+            throw new OverflowException();
+
+        return input;
+    }
+}
+
+public class ULongToDoubleConverter : IConverter<ulong, double>
+{
+    public double Convert(ulong input)
+    {
+        if (input > double.MaxValue)
+            throw new OverflowException();
+
+        return input;
+    }
+}
+
+public class ULongToDecimalConverter : IConverter<ulong, decimal>
+{
+    public decimal Convert(ulong input)
+    {
+        return input;
+    }
+}
+
+public class ULongToCharConverter : IConverter<ulong, char>
+{
+    public char Convert(ulong input)
+    {
+        if (input > char.MaxValue)
+            throw new OverflowException();
+
+        return (char)input;
+    }
+}
+
+public class ULongToBoolConverter : IConverter<ulong, bool>
+{
+    public bool Convert(ulong input)
+    {
+        return input != 0;
+    }
+}
+
+public class ULongToIntPtrConverter : IConverter<ulong, IntPtr>
+{
+    public IntPtr Convert(ulong input)
+    {
+        if (input > (ulong)IntPtr.MaxValue)
+            throw new OverflowException();
+
+        return (IntPtr)input;
+    }
+}
+
+public class ULongToUIntPtrConverter : IConverter<ulong, UIntPtr>
+{
+    public UIntPtr Convert(ulong input)
+    {
+        if (input > (ulong)UIntPtr.MaxValue)
+            throw new OverflowException();
+
+        return (UIntPtr)input;
+    }
+}
+
+public class ULongToStringConverter : IConverter<ulong, string>
+{
+    public string Convert(ulong input)
+    {
+        return input.ToString();
+    }
+}
